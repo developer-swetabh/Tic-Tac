e2c77d8b03635b54e099dcc267e22901128fdd01
swetabh <suman.swetabh@gmail.com>
2021-05-02 17:46:57 +0530

    Initial Commit

---
+++ Tic-Tac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tic-Tac
+//
+//  Created by Swetabh Suman on 02/05/21.
+//  Copyright © 2021 Swetabh Suman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
